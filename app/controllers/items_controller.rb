332398c8e849cf78f22116f63955fa1274f3cbf8
ian gottheim <iangottheim@gmail.com>
2022-10-14 13:12:58 -0400

    commit tree 06df6d366f9a079826a58f00f7fe51b426f35301

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def index
+    if params[:user_id]
+      items = User.find(params[:user_id]).items
+    else
     items = Item.all
-    render json: items, include: :user
   end
+    render json: items, include: :user
+
+end
+
+def create
+  if params[:user_id]
+    items = User.find(params[:user_id]).items.create(permitted_params)
+end
+  render json: items, status: :created
+end
+
+def show
+ item =  Item.find(params[:id])
+
+ render json: item
+ 
+end
+
+
+private
+
+def render_not_found_response
+  render json: { error: "Items not found" }, status: :not_found
+end
+
+def permitted_params
+  params.permit(:name, :description, :price)
+end
 
 end
